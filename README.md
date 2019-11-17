4e422102450cadc37e2bd82fd1e3fa4daa7f5565
shanelonergan <sptlonergan@gmail.com>
2019-11-16 19:50:00 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# hackerranck-js-practice-11-16
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/hackerranck-js-practice-11-16)
---
+++ index.js
@@ -0,0 +1,28 @@
+// Import stylesheets
+import './style.css';
+
+// Write Javascript code!
+function performOperation(secondInteger, secondDecimal, secondString) {
+    // Declare a variable named 'firstInteger' and initialize with integer value 4.
+    const firstInteger = 4;
+    
+    // Declare a variable named 'firstDecimal' and initialize with floating-point value 4.0.
+    const firstDecimal = 4.0;
+    
+    // Declare a variable named 'firstString' and initialize with the string "HackerRank".
+    const firstString = 'HackerRank ';
+    
+    // Write code that uses console.log to print the sum of the 'firstInteger' and 'secondInteger' (converted to a Number        type) on a new line.
+
+    const newInteger = parseInt(secondInteger, 10)
+    console.log(firstInteger + newInteger)
+    
+    // Write code that uses console.log to print the sum of 'firstDecimal' and 'secondDecimal' (converted to a Number            type) on a new line.
+
+    const newDecimal = parseFloat(secondDecimal, 10)
+    console.log(firstDecimal + newDecimal)
+    
+    // Write code that uses console.log to print the concatenation of 'firstString' and 'secondString' on a new line. The        variable 'firstString' must be printed first.
+
+    const newString = firstString + secondString
+    console.log(newString)
